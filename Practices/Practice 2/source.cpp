f4a205125a8a3d9c181a4df125c87438e10ec5d5
Frenzoid <frenzoid@protonmail.com>
2021-10-25 12:56:33 +0200

    updated pdf comments

--- Practices/Practice 2/source.cpp
+++ Practices/Practice 2/source.cpp
@@ -3,7 +3,6 @@
 #include <numeric>
 #include <chrono>
 #include <thread>
-#include <ctime>
 
 #define RED "\e[0;31m"
 #define CYN "\e[0;36m"
@@ -24,40 +23,24 @@ using namespace std;
 
 // ----------------------------------------------------------------------------
  
-Cells tablero = {
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,1,0,0,0,0,0,0,0,0,0,1, 0,0,0,0,0,0},
-    {0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}, 
-};
- 
-int numFilas = 10;
-int numCols = 20;
 
-unsigned start_time;
-unsigned end_time;
-double tottime;
+ 
+
  
 // ----------------------------------------------------------------------------
  
-int getNeighbor(int fila, int col, Cells& tablero) {
+int getNeighbor(int fila, int col, Cells& tablero, int numFilas, int numCols) {
     // use modulus to get wrapping effect at tablero edges
     return tablero.at((fila + numFilas) % numFilas).at((col + numCols) % numCols);
 }
 
-int sumarVecinos(int filas, int columnas, Cells& tablero){
+int sumarVecinos(int filas, int columnas, Cells& tablero, int numFilas, int numCols){
   int cuentaVecinos = 0;
   vector<int>deltas {-1, 0, 1};
   for (int col : deltas) {
     for (int fila : deltas){
       if (col || fila){
-        cuentaVecinos += getNeighbor(filas + fila, columnas + col, tablero);
+        cuentaVecinos += getNeighbor(filas + fila, columnas + col, tablero, numFilas, numCols);
       }
     }
   }
@@ -84,32 +67,52 @@ void mostrarCeldas(Cells tablero) {
     for (const Fila& fila : tablero) { mostrarFila(fila); }
 }
  
-int tick(Cells& tablero, int fila, int col) {
-    int cuenta = sumarVecinos(fila, col, tablero);
+int tick(Cells& tablero, int fila, int col, int numFilas, int numCols) {
+    int cuenta = sumarVecinos(fila, col, tablero, numFilas, numCols);
     bool birth = !tablero.at(fila).at(col) && cuenta == 3;
     bool survive = tablero.at(fila).at(col) && (cuenta == 2 || cuenta == 3);
     return birth || survive;
 }
  
-void updateCeldas(Cells& tablero) {
+void updateCeldas(Cells& tablero, int numFilas, int numCols) {
     Cells original = tablero;
     for (int fila = 0; fila < numFilas; fila++) {
         for (int col = 0; col < numCols; col++) {
-            tablero.at(fila).at(col) = tick(original, fila, col);
+            tablero.at(fila).at(col) = tick(original, fila, col, numFilas, numCols);
         }
+    }
+}
+
+void startGame(Cells tablero, int generaciones, int nFilas, int nCol, int timeWait){
+	for (int gen = 0; gen < generaciones; gen++) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(700));
+        printf("\033[2J");
+        printf("\033[%d;%dH", 0, 0);
+        std::cout << "\nGeneración " << gen << ":\n" << "\r";
+        mostrarCeldas(tablero);
+        updateCeldas(tablero, nFilas, nCol);
     }
 }
  
 int main () {
-    for (int gen = 0; gen < 20; gen++) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(700));
-        printf("\033[2J");
-        printf("\033[%d;%dH", 0, 0);
-        std::cout << "Tiempo: " << tottime << "ms \nGeneración " << gen << ":\n" << "\r";
-        mostrarCeldas(tablero);
-        start_time = clock();
-        updateCeldas(tablero);
-        end_time = clock();
-        tottime = double(end_time - start_time / CLOCKS_PER_SEC);
-    }
+
+	Cells tablero1 = {
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
+    {0,1,1,1,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+  };
+
+	int generacion = 20;
+	int numFilas = 10;
+	int numCols = 20;
+	int timeWait = 700; // ms
+
+	startGame(tablero1, generacion, numFilas, numCols, timeWait);
 }
