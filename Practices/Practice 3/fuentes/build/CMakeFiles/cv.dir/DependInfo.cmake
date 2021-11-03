f1483b2f70b07456fcc384ac67b3150620d0246f
Frenzoid <frenzoid@protonmail.com>
2021-11-03 10:44:38 +0100

    added new sources to practice 3

---
+++ Practices/Practice 3/fuentes/build/CMakeFiles/cv.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/crossValidation.cc" "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/build/CMakeFiles/cv.dir/crossValidation.cc.o"
+  "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/knn.cc" "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/build/CMakeFiles/cv.dir/knn.cc.o"
+  "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/main.cc" "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/build/CMakeFiles/cv.dir/main.cc.o"
+  "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/mnist.cc" "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/build/CMakeFiles/cv.dir/mnist.cc.o"
+  "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/sample.cc" "/home/frenzoid/Documents/UNI/UA_IC/Practices/Practice 3/fuentes/build/CMakeFiles/cv.dir/sample.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
